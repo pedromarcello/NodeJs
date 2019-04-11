04efbba41430bccb2af9dcfd1836d8a87062c014
pedro_marcello <pedro.marcello.q@gmail.com>
2019-04-10 23:28:40 -0300

    no message

--- src/server.js
+++ src/server.js
@@ -1,28 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const app = express();
-app.use(cors());
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
 
-const server = require('http').server(app)
+const app = express()
+
+const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const cor = require('cors')
-io.on('connection',socket =>{
-    socket.on('connectRoom',box=>{
-        socket.join(box);
+
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box)
     })
 })
-mongoose.connect("mongodb+srv://oministack:oministack@cluster0-jtvab.mongodb.net/boxes?retryWrites=true",{
-    useNewUrlParser: true
-});
 
-app.use((req,res,next)=>{
-    req.io=io;
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-7luqi.mongodb.net/test?retryWrites=true', {
+    useNewUrlParser: true,
+})
+
+
+app.use((req, res, next) => {
+    req.io = io
     return next()
-});
+})
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(require('./routes'));
-app.use('/files',express.static(path.resolve(__dirname,  '..', 'tmp')));
-server.listen(process.env.PORT ||3333);
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, "..", 'tmp')))
+
+app.use(require('./routes'))
+
+
+
+server.listen(process.env.PORT || 3333)
